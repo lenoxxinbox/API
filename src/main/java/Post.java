cdcb26ba790823ae5a40cb6b266ae5b966166377
Lenoxx in Box <lenoxxinbox@yandex.ru>
2022-09-24 22:49:05 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    public String name; // поле для имени
+    public String patronymic;
+    public String surname;
+    public String passport;
+    public String phone;
+    public boolean subscription;
+    public FormDate birthday;
+
+}
